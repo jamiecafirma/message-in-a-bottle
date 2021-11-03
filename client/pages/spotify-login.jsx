0e2cab96c62f88343da11715a05a9f6faf75e294
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-11-03 19:53:45 +0000

    Generated code-verifier #1

--- client/pages/spotify-login.jsx
+++ client/pages/spotify-login.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
 
 export default class SpotifyLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codeVerifier: ''
+    };
+    this.makeid = this.makeid.bind(this);
+    this.getRandomInt = this.getRandomInt.bind(this);
+    this.initiateSpotifyLogin = this.initiateSpotifyLogin.bind(this);
+  }
+
+  makeid(length) {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  initiateSpotifyLogin() {
+    const codeVerifier = this.makeid(this.getRandomInt(43, 128));
+    return codeVerifier;
+    // console.log(codeVerifier);
+  }
+
   render() {
     return (
       <>
@@ -8,7 +41,7 @@ export default class SpotifyLogin extends React.Component {
         <div className="row align-center flex-column position-absolute padding-2rem desktop-style">
         <h1 className="font-size-48 no-margin">Ahoy Matey!</h1>
           <h2 className="font-size-24 text-center">Click on the shell to connect your Spotify account</h2>
-        <a href="#"><img src="images/shell.png" className="width-80" /></a>
+        <a onClick={this.initiateSpotifyLogin} href="#"><img src="images/shell.png" className="width-80" /></a>
       </div>
       </>
     );
