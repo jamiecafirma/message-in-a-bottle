e3e018e9326be7a664b913292ea5ba692a9d7fa5
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-11-04 23:39:57 +0000

    Update schema.sql to create bottles and mementos tables #2

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,32 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."bottles" (
+	"bottleId" serial NOT NULL,
+	"messageTitle" TEXT NOT NULL,
+	"senderName" TEXT NOT NULL,
+	"recipientName" TEXT NOT NULL,
+	"recipientEmail" TEXT NOT NULL,
+	"playlistId" TEXT NOT NULL,
+	CONSTRAINT "bottles_pk" PRIMARY KEY ("bottleId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."mementos" (
+	"mementoId" serial NOT NULL,
+	"bottleId" serial NOT NULL,
+	"content" json NOT NULL,
+	"slideIndex" integer NOT NULL,
+	CONSTRAINT "mementos_pk" PRIMARY KEY ("mementoId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "mementos" ADD CONSTRAINT "mementos_fk0" FOREIGN KEY ("bottleId") REFERENCES "bottles"("bottleId");
