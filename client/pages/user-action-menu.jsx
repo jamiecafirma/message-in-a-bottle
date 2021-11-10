315fa52dfdb39cd04c25953aeef034bd8f18a959
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-11-10 01:30:17 +0000

    Fixed styling for UserActionMenu component #4

--- client/pages/user-action-menu.jsx
+++ client/pages/user-action-menu.jsx
@@ -8,10 +8,12 @@ export default class UserActionMenu extends React.Component {
         <div className="row align-center flex-column position-absolute padding-3rem absolute-center-horizontal">
           <h1 className="font-size-36 no-margin text-center">What would you like to do?</h1>
         </div>
-        <div className="row">
+        <div className="row user-action-menu position-absolute absolute-center-horizontal">
           <div className="column-half">
-            <h1 className="font-size-36 no-margin text-center">View</h1>
-            <img src="/images/message-in-a-bottle.png" className="width-140" />
+            <div className="row flex-column align-center">
+              <h1 className="font-size-36 no-margin text-center">View</h1>
+              <img src="/images/message-in-a-bottle.png" className="width-120" />
+            </div>
           </div>
         </div>
       </>
