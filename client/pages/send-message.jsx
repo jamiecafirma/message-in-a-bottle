98d8e93e5a8fa7e2f7df9d00b729345076906a5e
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-11-12 12:35:53 +0000

    Create a link that will send the user back to the UserActionMenu #7

--- client/pages/send-message.jsx
+++ client/pages/send-message.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class SendMessage extends React.Component {
   render() {
@@ -10,7 +10,9 @@ export default class SendMessage extends React.Component {
           <h1 className="font-size-48 no-margin">Send message to</h1>
           <h2 className="font-size-36 text-center">recipient@email.com?</h2>
           <a><img src="/images/wave.png" className="width-120" /></a>
-          <h1 className="font-size-36 no-margin text-center">Click the wave to confirm</h1>
+          <h1 className="font-size-36 no-margin text-center mb-24">Click the wave to confirm</h1>
+          <Link to="/menu"><img src="/images/parrot.png" className="width-120" /></Link>
+          <h1 className="font-size-36 no-margin text-center">Go back?</h1>
         </div>
       </>
     );
