e50c132a352b945f3e32ed622452d7c473e19ae6
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-11-12 13:41:50 +0000

    Send a POST request to 'api/send' with the bottleId in the request body, Create a link that will send the user back to the SpotifyLogin page #7

--- client/pages/send-message.jsx
+++ client/pages/send-message.jsx
@@ -7,42 +7,78 @@ export default class SendMessage extends React.Component {
     super(props);
     this.state = {
       bottleId: 0,
-      recipientEmail: null
+      recipientEmail: null,
+      emailSent: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ bottleId: this.context.currentBottleId });
+    const bottleId = this.context.currentBottleId;
+    this.setState({ bottleId: bottleId });
 
-    fetch(`/api/messages/${this.state.bottleId}`)
+    fetch(`/api/messages/${bottleId}`)
       .then(response => {
         return response.json();
       })
       .then(data => {
-        // console.log(data);
         const { recipientEmail } = data;
         this.setState({ recipientEmail: recipientEmail });
       })
       .catch(error => console.error('There was an unexpected error', error));
   }
 
+  handleClick(event) {
+    const sendId = { bottleId: this.state.bottleId };
+    const myInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(sendId)
+    };
+    fetch('/api/send', myInit)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ emailSent: true });
+      })
+      .catch(error => {
+        console.error('There was an unexpected error', error);
+      });
+  }
+
   render() {
     if (!this.state.recipientEmail) {
       return null;
+    } else if (!this.state.emailSent) {
+      return (
+        <>
+          <div className="overlay position-absolute"></div>
+          <div className="row align-center flex-column position-absolute padding-3rem desktop-style">
+            <h1 className="font-size-48 no-margin">Send message to</h1>
+            <h2 className="font-size-36 text-center">{`${this.state.recipientEmail}?`}</h2>
+            <a onClick={this.handleClick}><img src="/images/wave.png" className="width-120" /></a>
+            <h1 className="font-size-36 no-margin text-center mb-24">Click the wave to confirm</h1>
+            <Link to="/menu"><img src="/images/parrot.png" className="width-120" /></Link>
+            <h1 className="font-size-36 no-margin text-center">Go back?</h1>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="overlay position-absolute"></div>
+          <div className="row align-center flex-column position-absolute padding-3rem desktop-style">
+            <h1 className="font-size-48 no-margin">Email Sent!</h1>
+            <h2 className="font-size-36 text-center">{`${this.state.recipientEmail}?`}</h2>
+            <Link to="/"><img src="/images/parrot.png" className="width-120" /></Link>
+            <h1 className="font-size-36 no-margin text-center">Make another Bottle?</h1>
+          </div>
+        </>
+      );
     }
-    return (
-      <>
-        <div className="overlay position-absolute"></div>
-        <div className="row align-center flex-column position-absolute padding-3rem desktop-style">
-          <h1 className="font-size-48 no-margin">Send message to</h1>
-          <h2 className="font-size-36 text-center">{`${this.state.recipientEmail}?`}</h2>
-          <a><img src="/images/wave.png" className="width-120" /></a>
-          <h1 className="font-size-36 no-margin text-center mb-24">Click the wave to confirm</h1>
-          <Link to="/menu"><img src="/images/parrot.png" className="width-120" /></Link>
-          <h1 className="font-size-36 no-margin text-center">Go back?</h1>
-        </div>
-      </>
-    );
   }
 }
 
