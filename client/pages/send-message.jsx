f9762ce50ac576cab6e1714344e0133d43e541b3
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-11-12 12:48:57 +0000

    Send a GET request to 'api/messages/:bottleId' to get the recipient email #7

--- client/pages/send-message.jsx
+++ client/pages/send-message.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../lib/app-context';
 
 export default class SendMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bottleId: 0,
+      recipientEmail: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ bottleId: this.context.currentBottleId });
+
+    fetch(`/api/messages/${this.state.bottleId}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        // console.log(data);
+        const { recipientEmail } = data;
+        this.setState({ recipientEmail: recipientEmail });
+      })
+      .catch(error => console.error('There was an unexpected error', error));
+  }
+
   render() {
+    if (!this.state.recipientEmail) {
+      return null;
+    }
     return (
       <>
         <div className="overlay position-absolute"></div>
         <div className="row align-center flex-column position-absolute padding-3rem desktop-style">
           <h1 className="font-size-48 no-margin">Send message to</h1>
-          <h2 className="font-size-36 text-center">recipient@email.com?</h2>
+          <h2 className="font-size-36 text-center">{`${this.state.recipientEmail}?`}</h2>
           <a><img src="/images/wave.png" className="width-120" /></a>
           <h1 className="font-size-36 no-margin text-center mb-24">Click the wave to confirm</h1>
           <Link to="/menu"><img src="/images/parrot.png" className="width-120" /></Link>
@@ -18,3 +45,5 @@ export default class SendMessage extends React.Component {
     );
   }
 }
+
+SendMessage.contextType = AppContext;
